2e6aecb640f94551400e8067ec4549a56b8d275a
Criz1019 <59945248+Criz1019@users.noreply.github.com>
2020-02-02 16:11:07 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -60,10 +60,10 @@
           <!-- start of cards -->
           <div class="vertical_cards_container">
             <b-card-group deck>
-              <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+              <b-card title="" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
                 <b-card-text>
-                  This is a wider card with supporting text below as a natural lead-in to additional content.
-                  This content is a little bit longer.
+                  <strong>Listed by CYBERMETRICS Lab as one of the 2016 top colleges in the Philippines</strong>
+                    <p>(The ONLY ONE listed in the Olongapo City and Subic Freeport area.)</p>
                 </b-card-text>
                 <template v-slot:footer>
                   <small class="text-muted">Last updated 3 mins ago</small>
@@ -123,7 +123,6 @@
                             <p> Also, the annual family gross income must be P200,000 or less.</p>
                             <p><i>Based on DepEd Order No. 36, s. 2016:</i></p>
                         </b-card-text>
-                        <b-card-text>{{ text }}</b-card-text>
                       </b-card-body>
                     </b-collapse>
                   </b-card>
