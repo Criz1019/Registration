ec2e3b3de930b960326b1b252d6e69f6679937c9
Criz1019 <59945248+Criz1019@users.noreply.github.com>
2020-02-06 20:10:02 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,27 @@
 <template>
   <b-footer class="footer">
-    <p>
-      Copyright &copy
-      <!-- Displays the current year (YYYY)  -->
-      {{ getFullYear }}
-      COMTEQ Computer & Business College.
-    </p>
+    <div>
+      <h3>Developers!</h3>
+        <p>Ms. Hasmin Cuevas - Project Manager</p>
+        <p>Apid, Cristine Barrios</p>
+        <p>Mariano, Renz Austin Delos Reyes</p>
+        <p>Marty, John Christopher Lulu</p>
+    </div>
+
+    <div>
+      <h3>Contact Us!</h3>
+        <b-link href="https://www.facebook.com/comteqsbfz/?epa=SEARCH_BOX">
+          <img src="../assets/images/facebook.png" img-alt="Image" class="facebook_icon" img-top />
+        </b-link>
+        <p>1200 4th floor Savers Appliance, Rizal Ave</p>
+        <p>East Tapinac, Olongapo City, Zambales</P>
+        <p>Tel: +63(47)252-3335/www.comteq.edu.ph</p>
+    </div>
+      <p>
+        Copyright &copy
+        {{ getFullYear }}
+        COMTEQ Computer & Business College.
+      </p>
   </b-footer>
 </template>
 
